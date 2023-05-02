3c421d97b0c9ea3d1605bc088a9572927ed5ee6b
anbehindY <yanminthwin@gmail.com>
2023-05-02 19:21:00 +0630

    Remove unnecesary files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header.js";
+import ToDoLogic from "./components/ToDoLogic.js";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <div className="todos">
+        < Header style={{
+        padding: '20px 0',
+        lineHeight: '1.5em',
+        color: '#aeadad',
+        textAlign: 'center',
+      }}/>
+        < ToDoLogic/>
+      </div>
     </div>
   );
 }
