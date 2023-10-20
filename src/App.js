90e264c3c6709f289020d41cadccb3bd3b5f825d
anbehindY <yanminthwin@gmail.com>
2023-10-20 18:22:48 +0630

    Remove unnecessary

--- src/App.js
+++ src/App.js
@@ -1,18 +1,8 @@
-import Header from './components/Header';
-import ToDoLogic from './components/ToDoLogic';
-
 function App() {
   return (
     <div className="wrapper">
       <div className="todos">
-        <Header style={{
-          padding: '20px 0',
-          lineHeight: '1.5em',
-          color: '#aeadad',
-          textAlign: 'center',
-        }}
-        />
-        <ToDoLogic />
+        hello
       </div>
     </div>
   );
